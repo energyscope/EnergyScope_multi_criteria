647597a2aa06db41a820f328f1501216ec518a55
duboisa1 <antoine-dubois@hotmail.com>
2022-01-18 13:56:14 +0100

    Restructuring and pythonizing step 2 - modified example run

--- STEP_2_example/run_energyscope_example.py
+++ STEP_2_example/run_energyscope_example.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-This script modifies the input data and runs the EnergyScope model
+This script modifies the input data and runs the EnergyScopeTD STEP 2
 
 @author: Paolo Thiran, Matija Pavičević, Antoine Dubois
 """
-
 
 import yaml
 import os
@@ -30,18 +29,22 @@ if __name__ == '__main__':
 
     # Loading data
     all_data = es.import_data(config['user_data'], config['developer_data'])
+    # Modify the minimum capacities of some technologies
+    for tech in config['Technologies']['f_min']:
+        all_data['Technologies']['f_min'].loc[tech] = config['Technologies']['f_min'][tech]
 
     # Saving data to .dat files
-    out_path = f"{config['temp_dir']}/ESTD_data.dat"
-    es.print_estd(out_path, all_data, config["import_capacity"], config["GWP_limit"])
-    out_path = f"{config['temp_dir']}/ESTD_12TD.dat"
-    es.print_12td(out_path, all_data['Time_series'], config["step1_output"])
+    estd_path = f"{config['temp_dir']}/ESTD_data.dat"
+    es.print_estd(estd_path, all_data, config["system_limits"])
+    td12_path = f"{config['temp_dir']}/ESTD_12TD.dat"
+    es.print_12td(td12_path, all_data['Time_series'], config["step1_output"])
+    data_fns = [estd_path, td12_path]
 
     # Running EnergyScope
     cs = f"{config['case_studies_dir']}/{config['case_study_name']}"
-    run_fn = f"{config['ES_path']}/ESTD_main.run"
-    es.run_energyscope(cs, run_fn, config['AMPL_path'], config['temp_dir'])
+    mod_fns = [f"{config['ES_path']}/ESTD_model.mod"]
+    es.run_energyscope_new(cs, config['AMPL_path'], config["options"], mod_fns, data_fns, config['temp_dir'])
 
     # Example to print the sankey from this script
-    output_dir = f"{config['case_studies_dir']}/{config['case_study_name']}/output/"
-    es.drawSankey(path=f"{output_dir}/sankey")
+    # output_dir = f"{config['case_studies_dir']}/{config['case_study_name']}/output/"
+    # es.drawSankey(path=f"{output_dir}/sankey")
